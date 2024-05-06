e73e387ce2ef9e5b0313a02534bb2b7065503709
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-06 14:59:30 +0900

    複合代入演算子を使おう

---
+++ 22/app.js
@@ -0,0 +1,19 @@
+//足し算
+var num = 20;
+num = num + 10;
+console.log(num);
+var num1 = 20;
+num1 += 10;
+console.log(num1);
+//引き算
+var num2 = 20;
+num2 -= 10;
+console.log(num2);
+//掛け算
+var num3 = 20;
+num3 *= 10;
+console.log(num3);
+//割り算
+var num4 = 20;
+num4 /= 10;
+console.log(num4);
