b26367f282ab4e57eeea4c22d431a1624d92b3dd
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-06 14:46:26 +0900

    基本的な演算子を使おう

---
+++ 21/app.ts
@@ -0,0 +1,36 @@
+//足し算
+const ans =1+2;
+console.log(ans);
+
+const i=20;
+const j=10;
+const ans1 =i+j;
+console.log(ans1);
+
+//引き算
+const ans2=i-j;
+console.log(ans2);
+
+//掛け算
+const ans3=i*j;
+console.log(ans3);
+
+//割り算
+const ans4=i/j;
+console.log(ans4);
+
+const x=3;
+const y=2;
+const ans5=x/y;
+console.log(ans5);
+
+//余り
+const ans6=x%y;
+console.log(ans6);
+
+//べき乗
+const ans7 = x**y;
+console.log(ans7);
+
+const ans8= x*x;
+console.log(ans8);
