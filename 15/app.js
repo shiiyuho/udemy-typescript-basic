e275792aef6e26be7a6fd3babb23d29b2c4ee4f9
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-06 12:33:22 +0900

    エスケープシーケンスを使おう

---
+++ 15/app.js
@@ -0,0 +1,2 @@
+console.log('Hello I\'m Yuho Ishii.'); //シングルコートを文字として見てもらうには、前に「\」をつけること
+console.log(' \￥ \t AAA \n BBB'); //「\￥」￥そのものを表示　「\t」タブを表示　「\n」改行
---
+++ 15/app.ts
@@ -0,0 +1,9 @@
+ console.log('Hello I\'m Yuho Ishii.'); //シングルコートを文字として見てもらうには、前に「\」をつけること
+
+ console.log(' \￥ \t AAA \n BBB') //「\￥」￥そのものを表示　「\t」タブを表示　「\n」改行
+
+ console.log("Hello I'm Yuho Ishii."); //ダブルクオートでやればOK（シングルコートの時も同じ）
+
+ console.log('Hello');  //どちらでもOK（ぺージ内では統一しよう）
+ console.log("Hello");
+
