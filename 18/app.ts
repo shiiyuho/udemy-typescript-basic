d8999a7c79140f12683ef7804647ee9d86ff7f56
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-06 14:00:25 +0900

    変数名のつけ方を知ろう

---
+++ 18/app.ts
@@ -0,0 +1,16 @@
+let color;
+let colorName; //キャメルケース
+let myColorName;
+
+let favoriteFood = 20; //bad
+
+let _color;
+let $name;
+
+let name1;
+let _name1;
+
+let foo ='small';
+let Foo ='big';
+console.log(foo);
+console.log(Foo);
