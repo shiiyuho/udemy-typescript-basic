1053522bfb1e22b0fda758efe27ed65d36612386
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-07 15:12:47 +0900

    配列の基本を理解しよう

--- 29以降/dist/app.js
+++ 29以降/dist/app.js
@@ -1,6 +1,22 @@
 "use strict";
-//デフォルト値
-const getRectangle = (height, width = 10) => {
-    return height * width; //デフォルト値（右にしかつけられない）
-};
-console.log(getRectangle(3, 5));
+//配列
+const colors = ['Red', 'Green', 'Blue'];
+console.log(colors);
+// const num : string[]=[1,2,3];//型が違うからerror
+//型を書かない書き方（型推論）
+const iro = ['Red', 'Green', 'Blue']; //型が明らかな場合は書かなくてよい
+console.log(colors);
+//型がバラバラな場合の配列
+const mixdArr = ['abc', 1, true, false];
+console.log(mixdArr);
+//配列から値を取り出すときの書き方
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+//配列の数の要素を取得する書き方
+console.log(colors.length);
+//末尾の配列の値を取得する書き方
+console.log(colors[colors.length - 1]);
+//空の配列を宣言するには
+const emptyArr = [];
+console.log(emptyArr);
--- 29以降/src/app.ts
+++ 29以降/src/app.ts
@@ -1,7 +1,27 @@
-//デフォルト値
+//配列
+const colors : string[]=['Red','Green','Blue'];
+console.log(colors);
+// const num : string[]=[1,2,3];//型が違うからerror
 
-const getRectangle= (height:number,width:number = 10)=>{
-    return height*width;                       //デフォルト値（右にしかつけられない）
-};
+//型を書かない書き方（型推論）
+const iro =['Red','Green','Blue'];//型が明らかな場合は書かなくてよい
+console.log(colors);
 
-console.log(getRectangle(3,5));
+//型がバラバラな場合の配列
+const mixdArr=['abc',1,true,false];
+console.log(mixdArr);
+
+//配列から値を取り出すときの書き方
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+//配列の数の要素を取得する書き方
+console.log(colors.length);
+
+//末尾の配列の値を取得する書き方
+console.log(colors[colors.length-1]);
+
+//空の配列を宣言するには
+const emptyArr:number[]=[];
+console.log(emptyArr);
