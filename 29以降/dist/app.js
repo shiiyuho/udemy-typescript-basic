dfc24cb0d9d1778521e911e52e13981b00d940ed
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-07 13:40:19 +0900

    nullとundefinedを理解しよう

--- 29以降/dist/app.js
+++ 29以降/dist/app.js
@@ -1,26 +1,11 @@
 "use strict";
-//演習（swih）
-// let num = 1;
-//console.log(num); ←必須ではない
-//ランダム機能の場合
-let num = Math.floor(Math.random() * 5) + 1;
-switch (num) {
-    case 1:
-        console.log('大吉');
-        break;
-    case 2:
-        console.log('中吉');
-        break;
-    case 3:
-        console.log('小吉');
-        break;
-    case 4:
-        console.log('吉');
-        break;
-    case 5:
-        console.log('凶');
-        break;
-    default:
-        console.log('規定外の数値です。');
-        break; //break忘れずに！！
+let x;
+console.log(x);
+let item = null;
+console.log(item);
+if (x === undefined) {
+    console.log('undefined');
+}
+if (item === null) {
+    console.log('null');
 }
--- 29以降/src/app.ts
+++ 29以降/src/app.ts
@@ -1,27 +1,13 @@
-//演習（swih）
-// let num = 1;
-//console.log(num); ←必須ではない
+let x;
+console.log(x);
 
-//ランダム機能の場合
-let num = Math.floor(Math.random() * 5) + 1;
+let item=null;
+console.log(item);
 
-switch (num){
-    case 1:
-        console.log('大吉');
-        break;
-    case 2:
-        console.log('中吉');
-        break;
-    case 3:
-        console.log('小吉');
-        break;
-    case 4:
-        console.log('吉');
-        break;
-    case 5:
-        console.log('凶');
-        break;
-    default:
-        console.log('規定外の数値です。');
-        break;  //break忘れずに！！
+if(x===undefined){
+    console.log('undefined');
+}
+
+if(item===null){
+    console.log('null');
 }
