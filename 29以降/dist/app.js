42a139d24a50bd5dff65e45e3c0a03aa278f34b1
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-07 11:13:54 +0900

    様々な演算子を使ってみよう

--- 29以降/dist/app.js
+++ 29以降/dist/app.js
@@ -1,14 +1,31 @@
 "use strict";
-const x = 59;
-if (x >= 90) {
-    console.log('A');
+//以上
+const a = 100;
+if (a >= 100) {
+    console.log('100以上');
 }
-else if (x >= 80) {
-    console.log('B');
+//より大きい
+const b = 101;
+if (b > 100) {
+    console.log('100より大きい');
 }
-else if (x >= 60) {
-    console.log('C');
+//以下
+const c = 99;
+if (c <= 100) {
+    console.log('100以下');
 }
-else {
-    console.log('D');
+//未満
+const d = 99;
+if (d < 100) {
+    console.log('100未満');
+}
+//等しい
+const e = 100;
+if (e === 100) {
+    console.log('等しい');
+}
+//等しくない
+let f = 111;
+if (f !== 100) {
+    console.log('等しくない');
 }
--- 29以降/src/app.ts
+++ 29以降/src/app.ts
@@ -1,11 +1,35 @@
-const x = 59;
+//以上
+const a = 100;
+if(a>=100){
+    console.log('100以上');
+}
 
-if(x>=90){
-    console.log('A');
-}else if(x>=80){
-    console.log('B');
-}else if(x>=60){
-    console.log('C');
-}else{
-    console.log('D');
+//より大きい
+const b =101;
+if(b>100){
+    console.log('100より大きい');
+}
+
+//以下
+const c =99;
+if(c<=100){
+    console.log('100以下');
+}
+
+//未満
+const d =99;
+if(d<100){
+    console.log('100未満');
+}
+
+//等しい
+const e =100;
+if(e===100){
+    console.log('等しい');
+}
+
+//等しくない
+let f =111;
+if(f!==100){
+    console.log('等しくない');
 }
