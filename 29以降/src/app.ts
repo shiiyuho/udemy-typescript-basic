f5369acffd56a4d0ec65eb0bcc14fdec052049d6
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-07 16:52:55 +0900

    繰り返し処理do whileを使おう

--- 29以降/src/app.ts
+++ 29以降/src/app.ts
@@ -1,16 +1,5 @@
-//while文
-let i=1;  //変数の初期化　　　　　　繰
-while(i<=3){  //whileの条件式　　  り
-    console.log(i); //出力         返
-    i++;                         //し
-}
-
-//「j」が5になったらwihle文を抜ける
-let j=i;
-while (j<=10){
-    console.log(j);
-    if(j===5){
-        break;
-    }
-    j++;
-}
+let i=1; //変数の初期化
+do{
+    console.log(i);
+    i++
+}while(i<=3); //選別(継続or終了)
