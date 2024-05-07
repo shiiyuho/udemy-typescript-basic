006157c5fc8654e76f2ad9916d5d00130c29c879
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-07 17:03:17 +0900

    繰り返し処理for...ofを使おう

--- 29以降/dist/app.js
+++ 29以降/dist/app.js
@@ -1,6 +1,10 @@
 "use strict";
-let i = 1; //変数の初期化
-do {
-    console.log(i);
-    i++;
-} while (i <= 3); //選別(継続or終了)
+const colors = ['red', 'green', 'blue'];
+for (const color of colors) { //要素を一つづつ呼び出し
+    console.log(color);
+}
+//文字列の場合
+const str = 'TypSclipt';
+for (const character of str) {
+    console.log(character);
+}
