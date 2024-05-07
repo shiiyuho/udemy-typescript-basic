5d7e824277d2417a50adda606aaf88ebab913394
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-07 13:36:36 +0900

    演習：条件分岐　switch

--- 29以降/dist/app.js
+++ 29以降/dist/app.js
@@ -1,43 +1,26 @@
 "use strict";
-//Switc文
-// let num=6;
-// switch(num){
-//     case 1:
-//         console.log('Gold');
-//         break;
-//     case 2:
-//         console.log('Silver');
-//         break;
-//     case 3:
-//         console.log('Bronze');
-//         break;
-//     case 4:
-//         console.log('4th');
-//         break;
-//     case 5:
-//         console.log('5th');
-//         break;
-//     default:
-//         console.log('Other');
-//         break;
-// }
-//上のSwith文をIf文に書き換えた場合
-let num = 1;
-if (num === 1) {
-    console.log('Gold');
-}
-else if (num === 2) {
-    console.log('Silver');
-}
-else if (num === 3) {
-    console.log('Bronze');
-}
-else if (num === 4) {
-    console.log('4th');
-}
-else if (num === 5) {
-    console.log('5th');
-}
-else {
-    console.log('Other');
+//演習（swih）
+// let num = 1;
+//console.log(num); ←必須ではない
+//ランダム機能の場合
+let num = Math.floor(Math.random() * 5) + 1;
+switch (num) {
+    case 1:
+        console.log('大吉');
+        break;
+    case 2:
+        console.log('中吉');
+        break;
+    case 3:
+        console.log('小吉');
+        break;
+    case 4:
+        console.log('吉');
+        break;
+    case 5:
+        console.log('凶');
+        break;
+    default:
+        console.log('規定外の数値です。');
+        break; //break忘れずに！！
 }
