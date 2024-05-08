08ab66404b1b9fd1d103223b82bd2168f3b2ba65
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-08 13:56:21 +0900

    import宣言とexport宣言を利用しよう②

--- 29以降/src/app.ts
+++ 29以降/src/app.ts
@@ -1,3 +1,3 @@
 //インポート（引き落とし）
-import { name,age } from "./nakamura.js";
-console.log(name,age);
+import { myname,age as myage } from "./nakamura.js";
+console.log(myname,myage);
--- 29以降/src/nakamura.ts
+++ 29以降/src/nakamura.ts
@@ -1,3 +1,4 @@
-//エクスポート（排出）
-export const name='Nakamura';
-export const age=20;
+//エクスポート 省略版（排出）
+ const name='Nakamura';
+ const age=20;
+ export{name as myname,age};
