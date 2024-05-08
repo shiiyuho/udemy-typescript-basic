b8b3aab85d1e6feda7454aefe51dd82accb1039d
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-08 10:54:54 +0900

    import宣言とexport宣言を利用しよう①

--- 29以降/dist/app.js
+++ 29以降/dist/app.js
@@ -1,30 +1,3 @@
-"use strict";
-//コンストラクタのオーバーライド
-class User {
-    name = '';
-    age = 0;
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    isAdult() {
-        return this.age >= 18;
-    }
-}
-class AdminUser extends User {
-    adminRole = 1;
-    constructor(name, age, adminRole) {
-        super(name, age);
-        this.adminRole = adminRole;
-    }
-    sayAdminRole() {
-        console.log(`My adminRole is ${this.adminRole}`);
-    }
-    isAdult() {
-        return true;
-    }
-}
-const emma = new AdminUser('emma', 0, 2); //ここも変更「2」を追加してる
-console.log(emma.name);
-emma.sayAdminRole();
-console.log(emma.isAdult());
+//インポート（引き落とし）
+import { name, age } from "./nakamura.js";
+console.log(name, age);
---
+++ 29以降/src/nakamura.ts
@@ -0,0 +1,3 @@
+//エクスポート（排出）
+export const name='Nakamura';
+export const age=20;
