21a2b51d8490bf8d42fbe2439724d6de26a0ea82
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-08 09:24:19 +0900

    クラス宣言をしよう

--- 29以降/dist/app.js
+++ 29以降/dist/app.js
@@ -1,12 +1,14 @@
 "use strict";
-//スコアの平均を求める
-const score = [100, 90, 80, 70, 60];
-// console.log(score);
-let sum = 0;
-for (let i = 0; i < score.length; i++) {
-    sum += score[i];
+//クラス宣言
+class User {
+    name = '';
+    age = 0;
 }
-//平均値を求める
-const average = sum / score.length;
-console.log(`sum:${sum}`);
-console.log(`average:${average}`);
+//クラスのインスタンス化
+const olivia = new User();
+console.log(olivia.name);
+console.log(olivia.age);
+olivia.name = 'olivia';
+olivia.age = 23;
+console.log(olivia.name);
+console.log(olivia.age);
