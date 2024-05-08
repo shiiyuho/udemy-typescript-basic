bb068c29a1c7a64278577c41a1f961e225916bd5
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-08 09:45:15 +0900

    メソッドを使おう

--- 29以降/dist/app.js
+++ 29以降/dist/app.js
@@ -1,6 +1,23 @@
 "use strict";
-//プロパティを読み取り専用に
+//メソッド
 class User {
-    name = 'ishii'; //「readonly」を使うと読み取り専用になる
+    name = '';
     age = 0;
+    isAdult() {
+        return this.age >= 18; //Userの値が18以上なら「true」、以下なら「false」と出力される
+    }
+    setAge(age) {
+        this.age = age; //thisは自分を表す
+    }
 }
+const olivia = new User();
+console.log(olivia.age);
+console.log(olivia.isAdult());
+olivia.setAge(23);
+console.log(olivia.age);
+console.log(olivia.isAdult());
+//2つ目のインスタンス
+const liam = new User();
+liam.setAge(18);
+console.log(liam.age);
+console.log(liam.isAdult());
--- 29以降/src/app.ts
+++ 29以降/src/app.ts
@@ -1,5 +1,27 @@
-//プロパティを読み取り専用に
+//メソッド
 class User{
-    readonly name:string='ishii'; //「readonly」を使うと読み取り専用になる
+    name:string='';
     age:number=0;
+
+    isAdult():boolean{
+        return this.age>=18;    //Userの値が18以上なら「true」、以下なら「false」と出力される
+    }
+
+    setAge(age:number){
+        this.age=age;   //thisは自分を表す
+    }
 }
+
+const olivia=new User();
+console.log(olivia.age);
+console.log(olivia.isAdult());
+
+olivia.setAge(23);
+console.log(olivia.age);
+console.log(olivia.isAdult());
+
+//2つ目のインスタンス
+const liam=new User();
+liam.setAge(18);
+console.log(liam.age);
+console.log(liam.isAdult());
